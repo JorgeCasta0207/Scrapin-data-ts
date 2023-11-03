9dfdbe6c4ff4a8817e089f1b09b1d13030e967a0
Jorge Casta <jorgerochin0102@gmail.com>
2023-11-03 13:56:40 -0700

    Scraper Setup Complete, able to screenshot

--- dist/scraper.js
+++ dist/scraper.js
@@ -1,15 +1,15 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
-puppeteer.launch({ headless: true }).then(async (browser) => {
+// const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+// puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+puppeteer.launch({ headless: false }).then(async (browser) => {
     const page = await browser.newPage();
-    await page.setViewport({ width: 800, height: 600 });
-    console.log(`Testing adblocker plugin..`);
-    await page.goto('https://www.vanityfair.com');
-    await page.waitForTimeout(1000);
-    await page.screenshot({ path: 'adblocker.png', fullPage: true });
+    // await page.setViewport({ width: 800, height: 600 })
+    // console.log(`Testing adblocker plugin..`)
+    // await page.goto('https://www.vanityfair.com')
+    // await page.waitForTimeout(1000)
+    // await page.screenshot({ path: 'adblocker.png', fullPage: true })
     console.log(`Testing the stealth plugin..`);
     await page.goto('https://bot.sannysoft.com');
     await page.waitForTimeout(5000);
