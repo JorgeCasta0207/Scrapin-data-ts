fb7f956b7e26a5b66882e7e104fd0e778fd3643d
Jorge Casta <jorgerochin0102@gmail.com>
2023-11-07 15:52:50 -0800

    Trying to click on Local News Top Story link, (not yet working)

--- dist/scraper.js
+++ dist/scraper.js
@@ -2,44 +2,28 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 const { writeFile } = require('fs');
-const url = 'https://www.algonquincollege.com/';
-const searchTermCLI = process.argv.length > 2 ? process.argv[2] : 'Engineering';
+const { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } = require('puppeteer');
+const AdBlocker = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdBlocker({
+    interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY
+}));
+const url = 'https://www.modbee.com/news/local/';
 puppeteer.launch({ headless: false }).then(async (browser) => {
     const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(2 * 60 * 1000);
     await page.setViewport({ width: 800, height: 600 });
-    console.log(`Testing the stealth plugin..`);
-    await page.goto('https://www.algonquincollege.com/');
-    await page.waitForTimeout(5000);
-    await page.screenshot({ path: './mainpage.png ' });
-    const searchBTN = await page.waitForSelector('button.programSearchButton');
-    await page.type('input#programSearch', searchTermCLI, { delay: 100 });
-    await searchBTN.click();
-    await page.waitForNavigation({ waitUntil: 'load' });
-    await page.waitForSelector('table.programFilterList');
-    await page.screenshot({ path: './table.png', fullPage: true });
-    const data = await page.$$eval('table.programFilterList tbody tr', (rows) => {
-        return rows.map(row => {
-            if (row.classList.contains('odd') || row.classList.contains('even')) {
-                const tds = row.querySelectorAll('td');
-                return {
-                    name: tds[1].innerText,
-                    area: tds[2].innerText,
-                    campus: tds[3].innerText,
-                    length: tds[5].innerText,
-                };
-            }
-            else {
-                return null;
-            }
-        })
-            .filter((row) => row);
+    await page.goto(url);
+    await page.evaluate(() => {
+        const link = document.querySelector('h3.h1 a[href="https://www.modbee.com/news/local/article281504428.html#storylink=mainstage_lead"]');
+        if (link instanceof HTMLAnchorElement) {
+            link.click();
+        }
+        else {
+            console.error('Not an anchor element...');
+        }
     });
-    console.log({ data });
-    await writeFile('./data/courseDetails.JSON', JSON.stringify(data), 'utf-8', (err) => {
-        if (err)
-            throw err;
-        console.log('Data Has Been Saved :)');
-    });
-    console.log(`All done, check the screenshots. ✨`);
+    // await page.waitForNavigation({ waitUntil: 'load'});
+    await page.screenshot({ path: 'Test-page.jpg', fullPage: true });
+    console.log('All Done Pimp!✅');
     await browser.close();
 });
